29d6ddd4c6116f8a879600dc56c32616db02cdc7
ElenaMegia <emegican@myuax.com>
2022-11-21 19:48:55 +0100

    nuevo trabajo

---
+++ src/main/java/com/example/accesoadatos2/AccesoADatos2Application.java
@@ -0,0 +1,13 @@
+package com.example.accesoadatos2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccesoADatos2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccesoADatos2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/accesoadatos2/AccesoADatos2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accesoadatos2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccesoADatos2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
