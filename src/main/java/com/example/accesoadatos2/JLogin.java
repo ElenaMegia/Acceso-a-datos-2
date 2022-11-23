688b11f13f58c3d514dbc700c89729149d218f86
ElenaMegia <emegican@myuax.com>
2022-11-23 20:02:06 +0100

    creacion de ventana de login to fea

---
+++ src/main/java/com/example/accesoadatos2/JLogin.java
@@ -0,0 +1,13 @@
+package com.example.accesoadatos2;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class JLogin extends JDialog implements ActionListener {
+
+    private JPanel Panelgrande;
+    private JButton cancelarButton;
+    private JButton aceptarButton;
+    private JTextField tfUsuario;
+    private JPasswordField tfContraseña;
+}
---
+++ src/main/java/com/example/accesoadatos2/Ventana/Ventana.java
@@ -0,0 +1,9 @@
+package com.example.accesoadatos2.Ventana;
+
+public class Ventana {
+
+
+
+
+
+}
